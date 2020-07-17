80dc7c9221a231a3c65dc626203853b9db8abab6
cookieopjax <vigor01695@gmail.com>
2020-07-17 23:56:35 +0800

    http test

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'dart:convert';
 
-void main()=>runApp(MaterialApp(
-  home:Home(),
-));
+void main() => runApp(MyApp());
 
-class Home extends StatelessWidget {
-  final String host = 'https://jsonplaceholder.typicode.com/posts';
-  List datas = [];
-  String test = "123";
-
-  getData(){
-    http.get(host).then((response){
-     datas = jsonDecode(response.body);
-     print(datas.ty);
-
-    });
-  }
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    getData();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _text = '沒有資料';
+  void _getData() async {
+    String result = 'NULL';
+    try {
+      Response response =
+      await get('https://jsonplaceholder.typicode.com/posts');
+      List data = jsonDecode(response.body);
+      print(data[0]['title'].runtimeType);
+      result = data[0]['title'];
+    } catch (exception) {
+      result = '接收錯誤';
+    }
+    setState(() {
+      _text = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:Text(datas[2]["title"].toString())
-        ),
-      );
+        child: Text(_text),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.check),
+        onPressed: _getData,
+      ),
+    );
   }
 }
