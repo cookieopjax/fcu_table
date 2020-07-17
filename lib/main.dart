36ea7ae4df4e74636b34207359ea9f2cee6c4426
cookieopjax <vigor01695@gmail.com>
2020-07-17 22:10:48 +0800

    for jia tests http

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 void main()=>runApp(MaterialApp(
   home:Home(),
 ));
 
 class Home extends StatelessWidget {
+  final String host = 'https://jsonplaceholder.typicode.com/posts';
+  List datas = [];
+  String test = "123";
+
+  getData(){
+    http.get(host).then((response){
+     datas = jsonDecode(response.body);
+     print(datas.ty);
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
-        child:Text("testt"),
-      ),
-    );
+        child:Text(datas[2]["title"].toString())
+        ),
+      );
   }
 }
