bb362673a6b13fd0b5e17f6aae9347055b857a8b
cookieopjax <vigor01695@gmail.com>
2020-07-18 23:44:36 +0800

    #4 test login info

---
+++ lib/loginPage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NID Login"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  labelText: "Name *",
+                  hintText: "Your NID account username",
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  labelText: "Password *",
+                  hintText: "Your NID account password or ...",
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 52.0,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 48.0,
+              height: 48.0,
+              child: RaisedButton(
+                child: Text("Login"),
+                onPressed: () {
+
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'dart:io';
+import 'loginPage.dart';
 
 void main() => runApp(MyApp());
+
+String account;
+String password;
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
